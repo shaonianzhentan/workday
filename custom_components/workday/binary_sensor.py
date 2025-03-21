5047768d67e3dde3e41ce95a074d419053683f05
shaonianzhentan <2669757+shaonianzhentan@user.noreply.gitee.com>
2025-03-21 01:59:50 +0000

    支持读取农历日期

--- custom_components/workday/binary_sensor.py
+++ custom_components/workday/binary_sensor.py
@@ -1,10 +1,9 @@
-import time, requests
+import time, requests, sxtwl
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-
 from .const import SENSOR
 
 async def async_setup_entry(
@@ -28,11 +27,12 @@ class WorkdaySensor(BinarySensorEntity):
     async def async_update(self) -> None:
         today = time.localtime()
         if self.today is None or self.today.tm_mday != today.tm_mday:
+            year, month, day = today.tm_year, today.tm_mon, today.tm_mday
             is_on = False
             today_str = time.strftime('%Y-%m-%d', today)
             # 计算闰年
             year_day = 365            
-            if (today.tm_year % 100 == 0 and today.tm_year % 400 == 0) or today.tm_year % 4 == 0:
+            if (year % 100 == 0 and year % 400 == 0) or year % 4 == 0:
                 year_day = 366
             weeks = ['一', '二', '三', '四', '五', '六', '日']
             # 判断是否周一至周五
@@ -40,8 +40,8 @@ class WorkdaySensor(BinarySensorEntity):
                 is_on = True
             # 判断是否节假日
             try:
-                #url = f'http://chinese-holidays-data.basten.me/data/{today.tm_year}.json'
-                url = f'https://gitee.com/shaonianzhentan/workday/raw/main/data/{today.tm_year}.json'
+                #url = f'http://chinese-holidays-data.basten.me/data/{year}.json'
+                url = f'https://gitee.com/shaonianzhentan/workday/raw/main/data/{year}.json'
                 res = await self.hass.async_add_executor_job(requests.get, url)
                 holiday = res.json()
                 for holi in holiday:
@@ -54,12 +54,18 @@ class WorkdaySensor(BinarySensorEntity):
                         if today_str >= holi_range[0] and today_str <= holi_range[1]:
                             is_on = holi_type == 'workingday'
                 self._attr_is_on = is_on
+
+                # 读取农历时间
+                day = sxtwl.fromSolar(year, month, day)
                 self._attr_extra_state_attributes = {
                     '周': weeks[today.tm_wday],
                     '日期': today_str,
                     '今年天数': year_day,
                     '已过天数': today.tm_yday,
-                    '剩余天数': year_day - today.tm_yday
+                    '剩余天数': year_day - today.tm_yday,
+                    '农历年': day.getLunarYear(),
+                    '农历月': day.getLunarMonth(),
+                    '农历日': day.getLunarDay()
                 }
                 self.today = today
             except Exception as ex:
