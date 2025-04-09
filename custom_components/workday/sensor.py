8b5dae3947339bacaaf300da3fdac90a97bf6f1b
shaonianzhentan <2669757+shaonianzhentan@user.noreply.gitee.com>
2025-04-09 10:05:47 +0000

    更换依赖库

--- custom_components/workday/sensor.py
+++ custom_components/workday/sensor.py
@@ -1,4 +1,4 @@
-import sxtwl
+from lunar_python import Solar
 from datetime import datetime, date
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 from homeassistant.config_entries import ConfigEntry
@@ -14,7 +14,6 @@ async def async_setup_entry(
 ) -> None:
     async_add_entities([CalendarSensor('农历')], True)
 
-
 class CalendarSensor(SensorEntity):
 
     _attr_unique_id = SENSOR
@@ -27,8 +26,9 @@ class CalendarSensor(SensorEntity):
     async def async_update(self) -> None:
         now = datetime.now()
         if self.today is None or self.today.day != now.day:
-            lunar = sxtwl.fromSolar(now.year, now.month, now.day)
-            year, month, day = lunar.getLunarYear(), lunar.getLunarMonth(), lunar.getLunarDay()
+            solar = Solar.fromYmd(now.year, now.month, now.day)
+            lunar = solar.getLunar()
+            year, month, day = lunar.getYear(), lunar.getMonth(), lunar.getDay()
             self.native_value = date(year, month, day)
             self._attr_extra_state_attributes = {
                 '月': month,
